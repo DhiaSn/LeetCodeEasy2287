793117e01304ecfb0d6ae51f38987ab044ea2170
DhiaEddine.Snoussi <DhiaEddine.Snoussi@studentambassadors.com>
2024-07-31 13:21:42 +0100

    Add project files.

---
+++ LeetCodeEasy2287/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeEasy2287
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result1 = Solution.RearrangeCharacters("ilovecodingonleetcode", "code");
+            if (result1 != 2)
+            {
+                Console.WriteLine($"result1 is: {result1} instead of 2");
+                return;
+            }
+            int result2 = Solution.RearrangeCharacters("ilovecodingonleetcode", "code");
+            if (result2 != 1)
+            {
+                Console.WriteLine($"result2 is: {result2} instead of 1");
+                return;
+            }
+
+            int result3 = Solution.RearrangeCharacters("ilovecodingonleetcode", "code");
+            if (result3 != 1)
+            {
+                Console.WriteLine($"result3 is: {result3} instead of 1");
+                return;
+            }
+            Console.WriteLine($"it works perfectly!");
+        }
+    }
+}
---
+++ LeetCodeEasy2287/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeEasy2287
+{
+    public static class Solution
+    {
+        public static int RearrangeCharacters(string s, string target)
+        {
+            return 0; 
+        }
+    }
+}
