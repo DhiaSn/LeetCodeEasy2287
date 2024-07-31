b9eb10559aa2d2c5ed40387ef4f542a2d484fa1a
DhiaEddine.Snoussi <DhiaEddine.Snoussi@studentambassadors.com>
2024-07-31 16:24:16 +0100

    first uploaded solution

--- LeetCodeEasy2287/Program.cs
+++ LeetCodeEasy2287/Program.cs
@@ -10,19 +10,33 @@
                 Console.WriteLine($"result1 is: {result1} instead of 2");
                 return;
             }
-            int result2 = Solution.RearrangeCharacters("ilovecodingonleetcode", "code");
+            int result2 = Solution.RearrangeCharacters("abcba", "abc");
             if (result2 != 1)
             {
                 Console.WriteLine($"result2 is: {result2} instead of 1");
                 return;
             }
-
-            int result3 = Solution.RearrangeCharacters("ilovecodingonleetcode", "code");
+            int result3 = Solution.RearrangeCharacters("abbaccaddaeea", "aaaaa");
             if (result3 != 1)
             {
                 Console.WriteLine($"result3 is: {result3} instead of 1");
                 return;
             }
+
+            int result4 = Solution.RearrangeCharacters("codecodecodecode", "codecode");
+            if (result4 != 2)
+            {
+                Console.WriteLine($"result4 is: {result4} instead of 2");
+                return;
+            }
+
+            int result5 = Solution.RearrangeCharacters("lrnvlcqukanpdnluowenfxquitzryponxsikhciohyostvmkapkfpglzikitwiraqgchxnpryhwpuwpozacjhmwhjvslprqlnxrk", "woijih");
+            if (result5 != 2)
+            {
+                Console.WriteLine($"result5 is: {result5} instead of 2");
+                return;
+            }
+
             Console.WriteLine($"it works perfectly!");
         }
     }
--- LeetCodeEasy2287/Solution.cs
+++ LeetCodeEasy2287/Solution.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace LeetCodeEasy2287
 {
@@ -10,7 +6,63 @@ namespace LeetCodeEasy2287
     {
         public static int RearrangeCharacters(string s, string target)
         {
-            return 0; 
+            if(target.All(c => c == target[0]))
+            {
+                int result = 0;
+                int count = 0;
+
+                foreach (char c in s)
+                    if (c == target[0])
+                    {
+                        count++;
+                        if (target.Length == count)
+                        {
+                            result++;
+                            count = 0;
+                        }
+                    }
+
+                return result; 
+            }
+
+
+            char[] targetChars = target.ToCharArray();
+            List<char[]> allFound =
+            [
+                new char[targetChars.Length]
+            ]; 
+
+            foreach (char c in s)
+            {
+                if(targetChars.Contains(c))
+                {
+                    List<int> found = targetChars.Select((b, i) => b.Equals(c) ? i : -1).Where(i => i != -1).ToList();
+
+                    bool isAdded = false; 
+                    foreach (var item in allFound)
+                    {
+                        for (int i = 0; i < found.Count; i++)
+                        {
+                            if (item[found[i]] == '\0')
+                            {
+                                item[found[i]] = c;
+                                isAdded = true; 
+                                break;
+                            }
+                        }
+                        if (isAdded)
+                            break; 
+                    }
+                    if(!isAdded)
+                    {
+                        char[] temp = new char[targetChars.Length];
+                        temp[found[0]] = c;
+                        allFound.Add(temp);
+                    }
+                }
+            }
+
+            return allFound.Where(c => !c.Contains('\0')).Count();
         }
     }
 }
